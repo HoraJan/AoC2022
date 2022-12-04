a4efd9b61f08febbf8c111846392573d02c1d093
Jan Hora <jan.hora@saboit.de>
2022-12-04 07:51:16 +0100

    day 4

--- src/days/index.ts
+++ src/days/index.ts
@@ -1,7 +1,7 @@
 export * as day01 from './01';
 export * as day02 from './02';
 export * as day03 from './03';
-// export * as day04 from './04';
+export * as day04 from './04';
 // export * as day05 from './05';
 // export * as day06 from './06';
 // export * as day07 from './07';
