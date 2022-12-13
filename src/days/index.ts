0ec793ab5cce8a878410da9fa0790ddcb5209ecf
Jan Hora <jan.hora@sentinelone.com>
2022-12-13 10:31:42 +0100

    day 13

--- src/days/index.ts
+++ src/days/index.ts
@@ -10,7 +10,7 @@ export * as day09 from './09';
 export * as day10 from './10';
 export * as day11 from './11';
 export * as day12 from './12';
-// export * as day13 from './13';
+export * as day13 from './13';
 // export * as day14 from './14';
 // export * as day15 from './15';
 // export * as day16 from './16';
