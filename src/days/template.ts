957a79331cc1a46f755497d255ef558e89573c35
Jan Hora <jan.hora@sentinelone.com>
2022-12-13 10:32:59 +0100

    template file

---
+++ src/days/template.ts
@@ -0,0 +1,20 @@
+import { Test } from '.';
+
+const solve = (inputString: string) => {
+
+  return 0;
+};
+
+export const first = (inputString: string) => solve(inputString);
+
+export const second = (inputString: string) => solve(inputString);
+
+export const tests: Test[] = [{
+  input: ``,
+  results: {
+    first: 0,
+    second: 0,
+  },
+}];
+
+export const input = ``;
